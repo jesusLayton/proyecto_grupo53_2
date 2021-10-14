7e00d1aa5359fb0053453b7b7bfe4fcaf1b01f94
jesusLayton <jesuslayton92@gmail.com>
2021-10-14 00:19:26 -0500

    actualizacion modulo ventas

---
+++ tienda53/src/main/java/com/grupo53/tienda53/tienda_londono/BO/VentaController.java
@@ -0,0 +1,59 @@
+package com.grupo53.tienda53.tienda_londono.BO;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo53.tienda53.tienda_londono.DAO.VentaDAO;
+import com.grupo53.tienda53.tienda_londono.DTO.VentaVO;
+
+;
+
+@RestController
+public class VentaController {
+	
+	/*
+	 * @GetMapping obtener o buscar
+	 * @PostMapping insertar o agregar
+	 * @PutMapping modificar o actualizar
+	 * @DeleteMapping eliminar o borrar
+	 * */
+
+	@PostMapping("/registrarventa")
+	public void registrarVenta(VentaVO user) {
+		VentaDAO Dao = new VentaDAO();
+		Dao.registrarVenta(user);
+	}
+	
+	/*
+
+	@GetMapping("/consultarventa")
+	public ArrayList<VentaVO> consultarVentas(Integer consecutivo) {
+		VentaDAO Dao = new ventaDAO();
+		return Dao.consultarVenta(consecutivo);
+	}
+
+	@GetMapping("/listarventa")
+	public ArrayList<VentaVO> listaDeClientes() {
+		ClienteDAO Dao = new ClienteDAO();
+		return Dao.listaDeClientes();
+	}
+	
+	@DeleteMapping("/eliminarventa")
+	public void eliminarVenta(Integer consecutivo) {
+		VentaDAO Dao = new VentaDAO();
+		Dao.eliminarVenta(consecutivo);
+	}
+	
+	@PutMapping("/actualizarventa")
+	public void actualizarVenta(VentaVO user) {
+		VentaDAO Dao = new VentaDAO();
+		Dao.actualizarVenta(user);
+	}
+	*/
+
+}
---
+++ tienda53/src/main/java/com/grupo53/tienda53/tienda_londono/DAO/VentaDAO.java
@@ -0,0 +1,174 @@
+package com.grupo53.tienda53.tienda_londono.DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+import com.grupo53.tienda53.tienda_londono.DTO.VentaVO;
+
+/**
+ * Clase que permite el acceso a la base de datos
+ *
+ */
+
+public class VentaDAO {
+	/**
+	 * Permite registrar un Cliente nuevo
+	 * 
+	 * @param user
+	 */
+	public boolean registrarVenta(VentaVO user) {
+		//llama y crea una instancia de la clase encargada de hacer la conexión
+		Conexion conex = new Conexion();
+
+		try {
+			//sentencia que se ejecutara en la base de datos
+			Statement estatuto = conex.getConnection().createStatement();
+			
+			//String que contiene la sentencia insert a ejecutar
+			String sentencia = "INSERT INTO ventas VALUES(" 
+					+ user.getCodigo_venta() + "," + "'"
+					+ user.getIvaventa() + "'," + "'"					
+					+ user.getTotal_venta() + "'," + "'" 
+					+ user.getValor_venta() + "'," + "'" 
+					
+					+ "');";
+			
+			//se ejecuta la sentencia en la base de datos
+			estatuto.executeUpdate(sentencia);
+			//impresión en consola para verificación 
+			System.out.println("Registrado " + sentencia);
+			//cerrando la sentencia y la conexión
+			estatuto.close();
+			conex.desconectar();
+			
+			return true;
+
+		} catch (SQLException e) {
+			//si hay un error en el sql mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo insertar el cliente");
+			System.out.println(e.getMessage());
+			System.out.println(e.getErrorCode());
+			
+			return false;
+		} catch (Exception e) {
+			//si hay cualquier otro error mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo insertar el cliente");
+			System.out.println(e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+			return false;
+		}
+
+	}
+
+	/**
+	 * permite consultar el Cliente asociado al user enviado como parametro
+	 * 
+	 * @param documento
+	 * @return
+	 */
+	public ArrayList<VentaVO> consultarVenta(Integer codigo) {	
+		//lista que contendra el o los clientes obtenidos
+		ArrayList<VentaVO> listaventas = new ArrayList<VentaVO>();		
+		//instancia de la conexión
+		Conexion conex = new Conexion();
+		try {
+			//prepare la sentencia en la base de datos
+			PreparedStatement consulta = conex.getConnection()
+					.prepareStatement("SELECT * FROM ventas where codigo_venta = ? ");		
+			// se cambia el comodin ? por el dato que ha llegado en el parametro de la funcion
+			consulta.setInt(1, codigo);			
+			//ejecute la sentencia
+			ResultSet res = consulta.executeQuery();			
+			//cree un objeto basado en la clase entidad con los datos encontrados
+			if (res.next()) {
+				VentaVO Venta = new VentaVO();
+				Venta.setCodigo_venta(Integer.parseInt(res.getString("codigo_venta")));
+				Venta.setIvaventa(res.getInt("ivaventa"));
+				Venta.setTotal_venta(res.getInt("total_venta"));
+				Venta.setValor_venta(res.getInt("valor_venta"));
+				
+
+				listaventas.add(Venta);
+			}
+			//cerrar resultado, sentencia y conexión
+			res.close();
+			consulta.close();
+			conex.desconectar();
+
+		} catch (SQLException e) {
+			//si hay un error en el sql mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo consultar el cliente");
+			System.out.println(e.getMessage());
+			System.out.println(e.getErrorCode());
+		} catch (Exception e) {
+			//si hay cualquier otro error mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo consultar el cliente");
+			System.out.println(e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+		}
+		return listaventas;
+	}
+
+	/**
+	 * permite consultar la lista de todos las vetas
+	 * 
+	 * @return
+	 */
+	public ArrayList<VentaVO> listaVentas() {
+		//lista que contendra el o las ventas obtenidas
+		ArrayList<VentaVO> listaventas = new ArrayList<VentaVO>();
+		
+		//instancia de la conexión
+		Conexion conex = new Conexion();
+
+		try {
+			//prepare la sentencia en la base de datos
+			PreparedStatement consulta = conex.getConnection().prepareStatement("SELECT * FROM Ventas ORDER BY codigo_venta");
+			
+			//ejecute la sentencia
+			ResultSet res = consulta.executeQuery();
+			
+			//cree un objeto para cada encontrado en la base de datos basado en la clase entidad con los datos encontrados
+			while (res.next()) {
+				VentaVO Venta = new VentaVO();
+				Venta.setCodigo_venta(Integer.parseInt(res.getString("codigo_venta")));
+				Venta.setIvaventa(res.getInt("ivaventa"));
+				Venta.setTotal_venta(res.getInt("total_venta"));
+				Venta.setValor_venta(res.getInt("valor_venta"));
+				
+
+				listaventas.add(Venta );
+			}
+			
+			//cerrar resultado, sentencia y conexión
+			res.close();
+			consulta.close();
+			conex.desconectar();
+
+		} catch (SQLException e) {
+			//si hay un error en el sql mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo consultar todos los clientes");
+			System.out.println(e.getMessage());
+			System.out.println(e.getErrorCode());
+		} catch (Exception e) {
+			//si hay cualquier otro error mostrarlo
+			System.out.println("------------------- ERROR --------------");
+			System.out.println("No se pudo consultar todos los clientes");
+			System.out.println(e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+		}
+
+		return listaventas;
+	}
+
+	
+
+	
+
+}
